c4d73348cb99ce7c46cc55fec337b962b43d8636
Anon <ajeet.singh@lpu.in>
2024-07-09 15:26:23 +0530

    Binary rep

---
+++ BinaryRep.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+using namespace std;
+void binary(int n){
+  string s="";
+  while(n){
+    s=(char)(n%2+'0') +s;
+    n=n/2;
+  }
+  cout<<s;
+}
+int main()
+{
+std::ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int n;
+cin>>n;
+binary(n);
+int a = (n & ~(1<<3));
+cout<<endl;
+// toggle kth bit N = N^(i<<k)
+// check set kth bit N = N &(i<<k)
+// kth bit set N = N | (1<<k)
+// kth bit unset N = N & ~(1<<k)
+// count No of set bits
+//
+/* a=a^b;
+b=a^b;
+a=a^b;   */
+cout<<INT_MAX;
+binary(a);
+return 0;
+}
