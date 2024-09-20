48b2d0107a750d192f9700b7c08e048354295c71
Anon <ajeet.singh@lpu.in>
2024-09-20 15:56:17 +0530

    contd...

---
+++ 330/generateSubsetsSums.cpp
@@ -0,0 +1,94 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+const ll MOD = 998244353;
+const double eps = 1e-12;
+#define forn(a,e) for(ll i = a; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+const int INTMOD = 1e9+7;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+set<int>st;
+void generateSubsetsSum(v32 &arr,int sum,int ind,int &n,v32 &ans){
+  if(ind>=n){
+    ans.push_back(sum);
+    st.insert(sum);
+    return;
+  }
+  //include 
+    generateSubsetsSum(arr,sum+arr[ind],ind+1,n,ans);
+  //Exclude 
+    generateSubsetsSum(arr,sum,ind+1,n,ans);
+}
+void solve() {
+  int n;
+  cin>>n;
+  v32 arr(n);
+  forn(0,n) cin>>arr[i];
+  v32 ans;
+  generateSubsetsSum(arr,0,0,n,ans);
+  sort(ans.begin(),ans.end());
+  ll sum=0;
+  forn(0,ans.size()){
+    if(!(ans[i] &1)){
+      sum+=ans[i];
+    } 
+    // cout<<ans[i]<<" ";
+  }
+  cout<<sum<<endl;
+  for(auto &s:st){
+    cout<<s<<" ";
+  }
+}
+int main() {
+    fast_cin();
+    int tc = 1;
+    // cin >> tc;
+    while(tc--){
+       solve();
+    }
+    return 0;
+}
